3aa5e3e2dbe047c7f41c2c0f4b307a563216c449
Zeh Li <lizehanai@aliyun.com>
2017-07-28 10:45:09 +0800

    按照官方教程，左上角的标题栏应该有一个icon，但是本次没有，本分支用来搞定这个问题 According to the offical sites, there should be a icon on the left of Label which has been given before OnCreate(). This banch is to repair it.

--- Phoneword/MainActivity.cs
+++ Phoneword/MainActivity.cs
@@ -1,4 +1,5 @@
-﻿using Android.App;
+﻿using System.Collections.Generic;
+using Android.App;
 using Android.Widget;
 using Android.OS;
 using System;
@@ -9,6 +10,7 @@ namespace Phoneword
     [Activity(Label = "Phone Word", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        static readonly List<string> phoneNumbers = new List<string>();
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -20,38 +22,43 @@ namespace Phoneword
             EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
             Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
             Button callButton = FindViewById<Button>(Resource.Id.CallButton);
+            Button callHistoryButton = FindViewById<Button>(Resource.Id.CallHistoryButton);
 
             //Disable the "Call" button
             callButton.Enabled = false;
 
             //Add code to translate number
-            string translateNumber = string.Empty;
+            string translatedNumber = string.Empty;
 
             translateButton.Click += (object sender, EventArgs e) =>
               {
                 //Translate user's alphannumeric phone number to numeric
-                translateNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
-                  if (String.IsNullOrWhiteSpace(translateNumber))
+                translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                  if (String.IsNullOrWhiteSpace(translatedNumber))
                   {
                       callButton.Text = "Call";
                       callButton.Enabled = false;
                   }
                   else
                   {
-                      callButton.Text = "Call" + translateNumber;
+                      callButton.Text = "Call" + translatedNumber;
                       callButton.Enabled = true;
                   }
               };
             callButton.Click += (object sender, EventArgs e) =>
               {
-                //On "Call" button click, try to dial phone number.
-                var callDialog = new AlertDialog.Builder(this);
-                  callDialog.SetMessage("Call" + translateNumber + "?");
-                  callDialog.SetNegativeButton("Call", delegate
+                  //On "Call" button click, try to dial phone number.
+                  var callDialog = new AlertDialog.Builder(this);
+                  callDialog.SetMessage("Call" + translatedNumber + "?");
+                  callDialog.SetNeutralButton("Call", delegate
                   {
+                    // add dialed number to list of called numbers.
+                    phoneNumbers.Add(translatedNumber);
+                    // enable the Call History button
+                    callHistoryButton.Enabled = true;
                     //Create intent to dial phone
                     var calllntent = new Intent(Intent.ActionCall);
-                    calllntent.SetData(Android.Net.Uri.Parse("tel:" + translateNumber));
+                    calllntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
                     StartActivity(calllntent);
                   });
                   callDialog.SetNegativeButton("Cancel", delegate { });
@@ -59,6 +66,13 @@ namespace Phoneword
                 //Show the alert dialog to the user and wait for respoonse.
                 callDialog.Show();
               };
+            callHistoryButton.Click += (object sender, EventArgs e) =>
+            {
+                var intent = new Intent(this, typeof(CallHistoryActivity));
+                intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+                StartActivity(intent);
+            };
+
 
         }
     }
